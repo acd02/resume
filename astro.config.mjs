88ae99160045c7cf20a88caca4c5957b039e187f
acd02 <alex.acd02@gmail.com>
2026-01-17 19:39:19 +0100

    chore: bump to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 import mdx from '@astrojs/mdx'
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
